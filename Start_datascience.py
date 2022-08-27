87d4795c4cf51307ffa57e4a3b2051d0d4210da6
Bharat <tech.bharat88@gmail.com>
2022-08-27 14:32:02 +0000

    master27Aug

---
+++ Start_datascience.py
@@ -0,0 +1,9 @@
+from re import L
+import pandas as pd
+import openpyxl
+a = pd.DataFrame()
+
+l = ['Great Britain','China','Russia','United States','Korea','Japan', 'Germany']
+a = pd.DataFrame(l)
+
+print(l)
