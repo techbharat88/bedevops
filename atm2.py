b86199a98e9be7cfedf7f48774e4f2618189da29
Bharat <tech.bharat88@gmail.com>
2022-09-10 06:37:38 +0000

    09 Sep

---
+++ atm2.py
@@ -0,0 +1,23 @@
+a=raw_input("Hello, Please tell me your name ")
+print("Welcome, " + a)
+b=raw_input("Please choose your option from below\nTransaction\nBalance Enquiry\n")
+b=int(b)
+m=50000
+if b==1:
+    c=raw_input("please enter your PIN: ")
+    c=int(c)
+    if c==7866:
+        d=raw_input("please enter your amount ")
+        d=int(d)
+        print("collect your cash, Thanks\n")
+        print("your available balance is: ",m-d) 
+    else:
+        print("incorrect password, Try again\n")
+else:
+    c=raw_input("please enter your PIN: ")
+    c=int(c)
+    if c==7866:
+        print("Your available balance is ", m)
+
+print("Thanks a lot")
+
